ea6bd547c15034fa23562146194ae18b538b80d0
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2021-09-22 10:00:26 +0530

    Change File Name

--- pylib.py
+++ pylib.py
@@ -7,12 +7,22 @@ class Frame:
         self.area = area
 
         """
+        this list is containing all the default attribute that should be available in Component.
+        """
+        self.defaultAttributes = ['show', 'x', 'y']
+
+        """
         This list will be containing all the elements that user will add in the frame
         """
         self.elements = []
 
-    def addElement(self, element):
-        pass
+    def addComponents(self, element):
+        attributes = element.__dir__()
+
+        if 'show' in attributes and 'x' in attributes and 'y' in attributes:
+
 
     def show(self):
         pass
+
+
