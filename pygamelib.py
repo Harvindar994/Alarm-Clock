9393daffc82a6abd87c44acf6f990c9d232b9d0c
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2021-09-24 17:12:14 +0530

    Creating Frame

--- pygamelib.py
+++ pygamelib.py
@@ -2,9 +2,17 @@ import pygame
 
 
 class Frame:
-    def __init__(self, name, area):
+    def __init__(self, name, area, backgroundColor=None, partition=None):
         self.name = name
-        self.area = area
+        self.x, self.y, self.width, self.height = area
+
+        """
+        data structure of partition will be in percentage.
+        structure: {area in percentage: color, area in percentage: color}
+        note: partition will work to define diffrent area with diffrent color, and it will be in the vertical way.
+        """
+        self.partition = partition
+        self.backgroundColor = backgroundColor
 
         """
         this list is containing all the default attribute that should be available in Component.
@@ -24,11 +32,27 @@ class Frame:
                 print(f"'{attribute}' is not available in the give object")
                 return
 
+        # defining the actual position of element according to the frame.
+        element.x = self.x + element.x
+        element.y = self.y + element.y
+
         # here adding the element in the list.
         self.elements.append(element)
         return True
 
-    def show(self):
+    def rePartition(self):
         pass
+
+    def show(self):
+        if self.backgroundColor is not None:
+            if self.partition is None:
+                pass
+            else:
+                pass
+
+
+    def showElement(self):
+        for element in self.elements:
+            element.show()
 
 
